e4f826c5ee33b5658c769e275ca2b8e2c41d634b
parmsam <parmartsam@gmail.com>
2023-09-28 22:10:43 -0400

    ensure pkg data included

--- R/data.R
+++ R/data.R
@@ -1,5 +1,10 @@
 #' @title RWeekly Releases data
 #' @description R weekly provides weekly updates from the R community. Updates are in releases published on `rweekly.org`.
+#' \describe{
+#'   \item{title}{Release title}
+#'   \item{description}{Release description}
+#'   \item{image}{Main image path}
+#' }
 #' @format A data frame with over 30 columns
 #' @source <https://github.com/rweekly/rweekly.org/>
 "rweekly_data"
--- data-raw/data_refresh.R
+++ data-raw/data_refresh.R
@@ -1,3 +1,4 @@
 rweekly_data <- data_load()
 
-save(rweekly_data, file = "data/rweekly_data.rda", compress = "xz")
+# save(rweekly_data, file = "data/rweekly_data.rda", compress = "xz")
+usethis::use_data(rweekly_data, overwrite = T)
